1540dfc04dec6832f5b0332817843ecbe25763b6
Durgesh-05 <durgeshkumar.d@somaiya.edu>
2024-09-17 20:41:41 +0530

    Added even selector

--- recoil/src/App.jsx
+++ recoil/src/App.jsx
@@ -1,5 +1,10 @@
-import { useRecoilValue, RecoilRoot, useRecoilState } from 'recoil';
-import { countAtom } from './store/atoms/count';
+import {
+  useRecoilValue,
+  RecoilRoot,
+  useRecoilState,
+  useSetRecoilState,
+} from 'recoil';
+import { countAtom, evenSelector } from './store/atoms/count';
 
 function App() {
   return (
@@ -17,6 +22,7 @@ function Count() {
     <div>
       <CountRenderer />
       <Buttons />
+      <EvenCountRenderer />
     </div>
   );
 }
@@ -30,8 +36,13 @@ function CountRenderer() {
   );
 }
 
+function EvenCountRenderer() {
+  const isEven = useRecoilValue(evenSelector);
+  return <div>{isEven ? 'It is even' : ''}</div>;
+}
+
 function Buttons() {
-  const [count, setCount] = useRecoilState(countAtom);
+  const setCount = useSetRecoilState(countAtom);
   return (
     <div>
       <button onClick={() => setCount((prev) => prev + 1)}>Increase</button>
